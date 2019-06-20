97abf450ca5c125e702aa6d6acdf155f83cafb2e
BreandonPorras <brandon.porras.26@gmail.com>
2019-06-19 21:47:55 -0600

    actualizacion vista

--- resources/views/publications/publications.blade.php
+++ resources/views/publications/publications.blade.php
@@ -28,11 +28,7 @@
              
             
              <div class="" style="width: 18rem;">
-<<<<<<< HEAD
-                <img  class="card-img-top" src="{{ url('storage/imgPublications/' . $publication->img) }}" alt="">
-=======
                  <img  class="card-img-top" src="{{ url('storage/imgPublications/' . $publication->img) }}" alt="">
->>>>>>> master
                 <div class="card-body text-center">
                     <h5 class="card-title text-center text-black">{{ $publication->titulo }}</h5>            
                         <a href="{{ route('publications.show', $publication) }}" class="btn btn-success">View publication</a> 
