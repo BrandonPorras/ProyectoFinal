ca889912dee3667ba89905777d0647352fb33c3a
BreandonPorras <brandon.porras.26@gmail.com>
2019-06-20 19:22:31 -0600

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+//const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+
+
+const stylesSCSS = async() => {
+	// 1. Donde  estan los archivos s
+	// 2. Compilar el sass
+	// 3. Donde se almacena de css
+
+	await gulp
+		.src('./src/scss/**/*.scss')
+		.pipe(
+			sass({
+				outputStyle: 'expanded'
+			}).on('error', sass.logError)
+		)
+		.pipe(autoprefixer('last 2 versions'))
+		.pipe(gulp.dest('./dist/css'));
+};
+gulp.task('watch', () =>{
+    gulp.watch('./src/scss/**/*.scss',stylesSCSS);  
+});
+
+gulp.task('stylesSCSS', stylesSCSS);
+
+gulp.task('default',gulp.parallel('watch'), 'stylesSCSS');
+
+
+exports.stylesSCSS = stylesSCSS;
+
+
+
