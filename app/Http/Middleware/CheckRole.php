c44a6018c27b8993aaaad53dbeebdae527ed6e37
BreandonPorras <brandon.porras.26@gmail.com>
2019-07-02 19:56:34 -0600

    middleware

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace INTEGRATEITM\Http\Middleware;
+
+
+use Closure;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        
+    if (! $request->user()->hasRole($role)) {
+        return redirect()->route('publications.index');
+        
+    }
+    return $next($request);
+    }
+}
