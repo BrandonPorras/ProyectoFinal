727f358e724bac375cef7384c901eb54940b9569
BreandonPorras <brandon.porras.26@gmail.com>
2019-07-03 21:14:19 -0600

    seeder

--- database/seeds/PublicationsSeeder.php
+++ database/seeds/PublicationsSeeder.php
@@ -32,6 +32,270 @@ class PublicationsSeeder extends Seeder
         $publication->save();
 
 
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+        $publication = new Publication();
+        $publication->titulo  = 'titulo Prueba';        
+        $publication->text = 'Este texto representa la descripcion de una publicacion
+        texto solo de pruebas, texto solo de pruebas';        
+        $publication->categoria ='Others';         
+       
+
+        $publication->img=time() .'imagenPrueba.jpg';
+        $publication->state='1';
+
+        $user = User::where('name', 'admin')->first();
+        $publication->user_id=$user->id;
+       
+        $publication->user()->associate($user);     
+
+        $publication->save();
+
+
        
 
        
